d893c57b670ff3085367a046ed7fc0ae70adaf5e
Dongximing <dongxx1104@gmail.com>
2025-12-05 11:03:34 -0500

    add input

--- data/ministral3.py
+++ data/ministral3.py
@@ -10,7 +10,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 parser = argparse.ArgumentParser()
 parser.add_argument("--start_id", type=int, default=0, help="start index (inclusive)")
 parser.add_argument("--end_id", type=int, default=100, help="end index (exclusive)")
-parser.add_argument("--output", type=str, default="Qwen2.5-Math7B-Instruct-math500.json")
+parser.add_argument("--output", type=str, default="ministral-3-8B-Instruct-math500.json")
 args = parser.parse_args()
 
 start_id = args.start_id
@@ -22,7 +22,7 @@ print(f"▶ Running from {start_id} to {end_id - 1}")
 # -----------------------------
 # Load model
 # -----------------------------
-model_name = "Qwen/Qwen2.5-Math-7B-Instruct"
+model_name = "mistralai/Ministral-3-8B-Instruct-2512"
 device = "cuda:0"
 
 model = AutoModelForCausalLM.from_pretrained(
